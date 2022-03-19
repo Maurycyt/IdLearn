20d1020f11e5e9c833f2ba513a977f734cd7126e
Maurycy Wojda <maurycyadam@gmail.com>
2022-03-19 13:47:15 +0100

    Added a shell command execution mechanisms. Features include - executing single commands - creating temporary script files in the temporary file directory of the OS - executing those temporary scripts - and tests

---
+++ src/main/java/mimuw/idlearn/core/ShellExecutor.java
@@ -0,0 +1,182 @@
+package mimuw.idlearn.core;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
+/**
+ * Executes shell commands and creates executable shell scripts.
+ */
+public class ShellExecutor {
+	/**
+	 * Path to the system temporary file directory.
+	 */
+	private final static Path tmpDirPath = Path.of(System.getProperty("java.io.tmpdir"), "Idlearn");
+
+	/**
+	 * The default contents of a new temporary script file.
+	 */
+	private final static String defaultScriptContents = "#!/bin/bash\n\n";
+
+	/**
+	 * The temporary shell script file path.
+	 */
+	private final Path scriptFilePath;
+
+	/**
+	 * The temporary shell script file.
+	 */
+	private final File scriptFile;
+
+	/**
+	 * The object which will write to the script file.
+	 */
+	private FileWriter writer = null;
+
+	private void prepareWriter(boolean append) {
+		try {
+			if (writer != null) {
+				writer.close();
+			}
+			writer = new FileWriter(scriptFile, append);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Adds raw content to the end of the script file.
+	 * @param content The new content to be appended.
+	 */
+	private void addContent(String content) {
+		if (writer != null) {
+			try {
+				writer.write(content);
+				writer.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Creates a new ShellExecutor object with a temporary shell script file.
+	 */
+	public ShellExecutor() {
+		// the result file path, the value of which will later be assigned to scriptFilePath
+		Path tempPath = null;
+		// the temporary file directory
+		File tmpDirFile = tmpDirPath.toFile();
+		// creates the temporary file directory if necessary
+		tmpDirFile.mkdirs();
+		// prepares file permissions (700, hopefully)
+		Set<PosixFilePermission> perms = PosixFilePermissions.fromString("rwx------");
+		try {
+			// creates the temporary file in the directory (/tmp/Idlearn/ on ubuntu linux, for example).
+			tempPath = Files.createTempFile(tmpDirPath,"IdlearnSEScript", ".sh", PosixFilePermissions.asFileAttribute(perms));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (tempPath != null) {
+				scriptFilePath = tempPath;
+				scriptFile = scriptFilePath.toFile();
+			} else {
+				scriptFilePath = null;
+				scriptFile = null;
+			}
+		}
+
+		// Write to the file, adding "#!/bin/bash"
+		if (scriptFile != null) {
+			clearScript();
+		}
+	}
+
+	/**
+	 * Returns the default contents of a temporary script file.
+	 * @return The contents of the script file.
+	 */
+	public static String getDefaultScriptContents() {
+		return defaultScriptContents;
+	}
+
+	/**
+	 * Returns the temporary script file.
+	 * @return The script file.
+	 */
+	public File getScriptFile() {
+		return scriptFile;
+	}
+
+	/**
+	 * Returns the path to the temporary script file.
+	 * @return The path to the script file.
+	 */
+	public Path getScriptFilePath() {
+		return scriptFilePath;
+	}
+
+	/**
+	 * Adds a given command to the end of the script file.
+	 * @param command The command to be added.
+	 */
+	public void addLineToScript(String command) {
+		addContent(command + "\n");
+	}
+
+	public void clearScript() {
+		prepareWriter(false);
+		addContent(defaultScriptContents);
+	}
+
+	/**
+	 * Executes the given command and returns its output.
+	 * @param command The command to execute.
+	 * @return The output of the executed command. Often empty.
+	 */
+	public String execute(String command) {
+		// Code copied from https://stackoverflow.com/q/26830617/14406682.
+		StringBuilder output = new StringBuilder();
+
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			String line;
+			while ((line = reader.readLine())!= null) {
+				output.append(line);
+				output.append("\n");
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
+	}
+
+	/**
+	 * Executes the created script.
+	 */
+	public String execute() {
+		String output = "";
+		String command = scriptFilePath.toString();
+
+		try {
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return output;
+		}
+
+		output = execute(command);
+		prepareWriter(true);
+		return output;
+	}
+}
