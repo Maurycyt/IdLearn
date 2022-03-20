dc2976ae343e93db3b9aaf656353966429496fb3
Kacper Chętkowski <kc432959@students.mimuw.edu.pl>
2022-03-20 19:22:28 +0100

    Commented code

--- src/main/java/mimuw/idlearn/core/State.java
+++ src/main/java/mimuw/idlearn/core/State.java
@@ -1,15 +1,6 @@
 package mimuw.idlearn.core;
 
-public abstract class State{
-	State(StateMachine stateMachine){
-		stateMachine_ = stateMachine;
-	}
-	
-	public abstract void activate();
-	public abstract void deactivate();
-	
-	protected StateMachine getStateMachine(){
-		return stateMachine_;
-	}
-	private final StateMachine stateMachine_;
+public interface State{
+	void activate();
+	void deactivate();
 }
--- src/main/java/mimuw/idlearn/core/StateMachine.java
+++ src/main/java/mimuw/idlearn/core/StateMachine.java
@@ -2,11 +2,22 @@ package mimuw.idlearn.core;
 
 import java.util.Stack;
 
+/**
+ * Implementation of a State Machine design pattern.
+ * Allows for safe managment of states.
+ * 
+ * Each operation on State Machine is lazy. 
+ * Actions only take place once the 'get' function is executed.
+ */
 public class StateMachine implements Listener{
-	StateMachine(){
+	public StateMachine(){
 		emitter.connect(this);
 	}
 	
+	/**
+	 * Add a state to the state machine
+	 * @param state new state
+	 */
 	public void add(State state){
 		emitter.notify((StateEvent)() -> {
 			if(!states.empty())
@@ -16,6 +27,9 @@ public class StateMachine implements Listener{
 		});
 	}
 	
+	/**
+	 * Remove top state from the state machine
+	 */
 	public void pop(){
 		emitter.notify((StateEvent)() -> {
 			states.pop().deactivate();
@@ -24,11 +38,18 @@ public class StateMachine implements Listener{
 		});
 	}
 	
+	/**
+	 * Replace top state with new state
+	 * @param state new state
+	 */
 	public void replace(State state){
 		pop();
 		add(state);
 	}
 	
+	/**
+	 * Remove all states from the state machine
+	 */
 	public void clear(){
 		emitter.notify((StateEvent)() -> {
 			while(!states.empty()){
@@ -39,7 +60,11 @@ public class StateMachine implements Listener{
 		});
 	}
 	
-	State get(){
+	/**
+	 * Execute queued events and return top state
+	 * @return current state
+	 */
+	public State get(){
 		emitter.processEvents();
 		if(states.empty())
 			throw new RuntimeException("No active State in StateMachine");
