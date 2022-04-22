41dbd383164f1ebac094ef05341f3b8faed22434
Maurycy Wojda <maurycyadam@gmail.com>
2022-04-22 19:20:53 +0000

    Package manager

--- src/test/java/mimuw/idlearn/GUI/coding/BlockIOTest.java
+++ src/test/java/mimuw/idlearn/GUI/coding/BlockIOTest.java
@@ -9,7 +9,6 @@ import mimuw.idlearn.language.base.Expression;
 import mimuw.idlearn.language.environment.Scope;
 import mimuw.idlearn.problems.PackageManager;
 import mimuw.idlearn.problems.ProblemPackage;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,86 +16,87 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class BlockIOTest {
 
-    private static void preparePlatform() {
-        try {
-            Platform.startup(() -> {
-            });
-        }
-        catch (IllegalStateException e) {
-            // Toolkit already initialized
-        }
-    }
+	private static void preparePlatform() {
+		try {
+			PackageManager.reloadProblemPackages();
+			Platform.startup(() -> {
+			});
+		}
+		catch (IllegalStateException e) {
+			// Toolkit already initialized
+		}
+	}
 
-    @Test
-    public void testIncorrectInput() {
-        preparePlatform();
-        try {
-            ProblemPackage pkg = PackageManager.getProblemPackage("Addition");
-            pkg.prepareTest(123);
+	@Test
+	public void testIncorrectInput() {
+		preparePlatform();
+		try {
+			ProblemPackage pkg = PackageManager.getProblemPackage("Addition");
+			pkg.prepareTest(123);
 
-            Group root = new Group();
-            CodeBox codeBox = new CodeBox();
+			Group root = new Group();
+			CodeBox codeBox = new CodeBox();
 
-            root.getChildren().add(codeBox);
+			root.getChildren().add(codeBox);
 
-            Read r1 = new Read(pkg);
-            Read r2 = new Read(pkg);
-            Read r3 = new Read(pkg);
-            r1.setText("x");
-            r2.setText("x");
-            r3.setText("x");
+			Read r1 = new Read(pkg);
+			Read r2 = new Read(pkg);
+			Read r3 = new Read(pkg);
+			r1.setText("x");
+			r2.setText("x");
+			r3.setText("x");
 
-            codeBox.addChild(0, r1);
-            codeBox.addChild(0, r2);
-            codeBox.addChild(0, r3);
+			codeBox.addChild(0, r1);
+			codeBox.addChild(0, r2);
+			codeBox.addChild(0, r3);
 
-            try {
-                Expression<Void> program = codeBox.compile();
-								Scope scope = new Scope();
-								program.evaluate(scope);
-                fail();
-            }
-            catch (Exception e) {}
-        }
-        catch (Exception e) {
-            fail();
-        }
-    }
+			try {
+				Expression<Void> program = codeBox.compile();
+				Scope scope = new Scope();
+				program.evaluate(scope);
+				fail();
+			}
+			catch (Exception ignored) {}
+		}
+		catch (Exception e) {
+			fail(e);
+		}
+	}
 
-    @Test
-    public void testAddition() {
-        preparePlatform();
-        try {
-            ProblemPackage pkg = PackageManager.getProblemPackage("Addition");
-            pkg.prepareTest(123);
+	@Test
+	public void testAddition() {
+		preparePlatform();
+		try {
+			ProblemPackage pkg = PackageManager.getProblemPackage("Addition");
+			pkg.prepareTest(123);
 
-            Group root = new Group();
-            CodeBox codeBox = new CodeBox();
+			Group root = new Group();
+			CodeBox codeBox = new CodeBox();
 
-            root.getChildren().add(codeBox);
-            Read r1 = new Read(pkg);
-            Read r2 = new Read(pkg);
-            r1.setText("x");
-            r2.setText("y");
+			root.getChildren().add(codeBox);
+			Read r1 = new Read(pkg);
+			Read r2 = new Read(pkg);
+			r1.setText("x");
+			r2.setText("y");
 
-            Add add = new Add();
-            add.setText("x", "x", "y");
+			Add add = new Add();
+			add.setText("x", "x", "y");
 
-            Write w = new Write(pkg);
-            w.setText("x");
+			Write w = new Write(pkg);
+			w.setText("x");
 
-            codeBox.addChild(0, w);
-            codeBox.addChild(0, add);
-            codeBox.addChild(0, r2);
-            codeBox.addChild(0, r1);
+			codeBox.addChild(0, w);
+			codeBox.addChild(0, add);
+			codeBox.addChild(0, r2);
+			codeBox.addChild(0, r1);
 
-            Expression<Void> program = codeBox.compile();
-						Scope scope = new Scope();
-						program.evaluate(scope);
-            assertTrue(pkg.checkTest());
-        }
-        catch (Exception e) {
-            fail();
-        }
-    }
+			Expression<Void> program = codeBox.compile();
+			Scope scope = new Scope();
+			program.evaluate(scope);
+			assertTrue(pkg.checkTest());
+		}
+		catch (Exception e) {
+			fail(e);
+		}
+	}
 }
