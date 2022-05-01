f4dba313f6a1b6709cf115f9157fd85371cb9ba6
Maurycy Wojda <maurycyadam@gmail.com>
2022-05-01 17:47:16 +0000

    Prepares packages for extension.

--- src/main/java/mimuw/idlearn/language/base/Variable.java
+++ src/main/java/mimuw/idlearn/language/base/Variable.java
@@ -27,19 +27,4 @@ public class Variable<T> implements Expression<T> {
 	public Value<T> evaluate(Scope scope) throws RuntimeException {
 		return scope.getVariable(name);
 	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		if (!super.equals(o))
-			return false;
-
-		Variable<T> other = (Variable<T>) o;
-
-		return name.equals(other.name);
-	}
-
 }
