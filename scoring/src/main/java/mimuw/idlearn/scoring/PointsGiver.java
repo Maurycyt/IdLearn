3d932f9922deb3b853321b123836a05af35f0c8d
Antoni <a.puch@student.uw.edu.pl>
2022-05-04 11:41:23 +0200

    Added concurrency management

--- scoring/src/main/java/mimuw/idlearn/scoring/PointsGiver.java
+++ scoring/src/main/java/mimuw/idlearn/scoring/PointsGiver.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Semaphore;
 
 public class PointsGiver {
 	private static final long POINTS_PER_TICK = 10;
 	private static final Timer givingTimer = new Timer();
 	private static final Map<String, PointsTimerTask> givingTasks = new HashMap<>();
+	private static final Map<String, Long> timeStamps = new HashMap<>();
+	private static final Semaphore mutex = new Semaphore(1);
 
 	private static class PointsTimerTask extends TimerTask {
 		long timeMillis;
@@ -27,7 +30,20 @@ public class PointsGiver {
 		}
 	}
 
-	public static void setSolutionSpeed(String problem, long timeInMillis) {
+	public static void setSolutionSpeed(String problem, long timeInMillis, long timeStamp) {
+
+		try {
+			mutex.acquire();
+		}
+		catch (InterruptedException e) {
+			throw new RuntimeException("Process interrupted");
+		}
+
+		Long oldStamp = timeStamps.get(problem);
+		if (oldStamp != null && oldStamp > timeStamp) {
+			mutex.release();
+			return;
+		}
 
 		PointsTimerTask task = givingTasks.get(problem);
 
@@ -38,12 +54,45 @@ public class PointsGiver {
 		task = new PointsTimerTask(timeInMillis);
 		task.start();
 		givingTasks.put(problem, task);
+		timeStamps.put(problem, timeStamp);
+
+
+		mutex.release();
 	}
 
 	public static void resetSolution(String problem) {
+
+		try {
+			mutex.acquire();
+		}
+		catch (InterruptedException e) {
+			throw new RuntimeException("Process interrupted");
+		}
+
 		PointsTimerTask task = givingTasks.get(problem);
 		if (task != null) {
 			task.cancel();
 		}
+		givingTasks.remove(problem);
+		timeStamps.remove(problem);
+
+		mutex.release();
+	}
+	public static void resetSolutions() {
+
+		try {
+			mutex.acquire();
+		}
+		catch (InterruptedException e) {
+			throw new RuntimeException("Process interrupted");
+		}
+
+		for (Map.Entry<String, PointsTimerTask> entry : givingTasks.entrySet()) {
+			entry.getValue().cancel();
+			timeStamps.remove(entry.getKey());
+			givingTasks.remove(entry.getKey(), entry.getValue());
+		}
+
+		mutex.release();
 	}
 }
--- scoring/src/test/java/mimuw/idlearn/scoring/PointsGiverTest.java
+++ scoring/src/test/java/mimuw/idlearn/scoring/PointsGiverTest.java
@@ -10,8 +10,10 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class PointsGiverTest {
 	@Test
 	public void testPointsGiver() {
+		PointsGiver.resetSolutions();
 		PointsManager.setPoints(0);
-		PointsGiver.setSolutionSpeed("a", 10);
+
+		PointsGiver.setSolutionSpeed("a", 10, 0);
 		try {
 			TimeUnit.MILLISECONDS.sleep(20);
 		}
@@ -21,7 +23,7 @@ public class PointsGiverTest {
 		long points1 = PointsManager.showPoints();
 		assertTrue(points1 > 0);
 
-		PointsGiver.setSolutionSpeed("a", 1);
+		PointsGiver.setSolutionSpeed("a", 1, 1);
 		try {
 			TimeUnit.MILLISECONDS.sleep(20);
 		}
@@ -32,10 +34,10 @@ public class PointsGiverTest {
 		long points2 = PointsManager.showPoints();
 		assertTrue(points2 > 5 * points1);
 
-		PointsGiver.resetSolution("a");
+		PointsGiver.resetSolutions();
 		PointsManager.setPoints(0);
 
-		PointsGiver.setSolutionSpeed("a", 10);
+		PointsGiver.setSolutionSpeed("a", 10, 0);
 		try {
 			TimeUnit.MILLISECONDS.sleep(20);
 		}
@@ -43,7 +45,7 @@ public class PointsGiverTest {
 			fail();
 		}
 
-		PointsGiver.setSolutionSpeed("b", 1);
+		PointsGiver.setSolutionSpeed("b", 1, 1);
 		try {
 			TimeUnit.MILLISECONDS.sleep(20);
 		}
@@ -56,4 +58,36 @@ public class PointsGiverTest {
 		assertTrue(points3 > points2);
 	}
 
+	@Test
+	public void testTimeStamps() {
+		PointsGiver.resetSolutions();
+		PointsManager.setPoints(0);
+
+		PointsGiver.setSolutionSpeed("a", 1, 1);
+		PointsGiver.setSolutionSpeed("a", 1000, 0);
+
+		try {
+			TimeUnit.MILLISECONDS.sleep(20);
+		}
+		catch (InterruptedException e) {
+			fail();
+		}
+
+		assertTrue(PointsManager.showPoints() > 10);
+
+		PointsGiver.resetSolutions();
+		PointsManager.setPoints(0);
+
+		PointsGiver.setSolutionSpeed("a", 1, 0);
+		PointsGiver.setSolutionSpeed("a", 1000, 1);
+
+		try {
+			TimeUnit.MILLISECONDS.sleep(20);
+		}
+		catch (InterruptedException e) {
+			fail();
+		}
+
+		assertTrue(PointsManager.showPoints() < 5);
+	}
 }
