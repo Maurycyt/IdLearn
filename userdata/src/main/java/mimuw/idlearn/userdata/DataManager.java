fa9279d8c17c3c52e3be66e38293b8e7875d784b
Antoni <a.puch@student.uw.edu.pl>
2022-05-06 12:42:23 +0200

    Added saving and loading

--- userdata/src/main/java/mimuw/idlearn/userdata/DataManager.java
+++ userdata/src/main/java/mimuw/idlearn/userdata/DataManager.java
@@ -2,12 +2,19 @@ package mimuw.idlearn.userdata;
 
 import mimuw.idlearn.core.Emitter;
 import mimuw.idlearn.core.Listener;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DataManager {
-	static final const long AUTOSAVE_TIMER = 30000;
+
+	private static final String SAVE_LOCATION = "/user.savedata";
+	private static final long AUTOSAVE_TIMER = 30000;
+	private static final Timer autosave_timer = new Timer();
 
 	// Points
 	private static long points = 0;
@@ -42,7 +49,7 @@ public class DataManager {
 
 	// Tasks
 	private static ArrayList<String> unlockedTasks;
-	public static void unlockTask(String task) {
+	public static void unlockTask(String task) throws IOException {
 		unlockedTasks.add(task);
 		saveData();
 	}
@@ -50,18 +57,51 @@ public class DataManager {
 		return new ArrayList<>(unlockedTasks);
 	}
 
-	private static class Data implements Serializable {
+	private static class Data {
 		long points;
 		ArrayList<String> unlockedTasks;
 	}
-	private static void saveData() {
+	private static void saveData() throws IOException {
+
+
+		File saveFile = new File(SAVE_LOCATION);
+
+		Gson gson = new Gson();
+
+		Data data = new Data();
+		data.points = points;
+		data.unlockedTasks = unlockedTasks;
+
+		FileWriter writer = new FileWriter(SAVE_LOCATION);
+		gson.toJson(data, writer);
 
 	}
-	private static void loadData() {
+	private static void loadData() throws IOException {
+		Gson gson = new Gson();
+		JsonReader reader = new JsonReader(new FileReader(SAVE_LOCATION));
+		Data data = gson.fromJson(reader, Data.class);
+		points = data.points;
+		unlockedTasks = data.unlockedTasks;
+	}
+
+	private static void setupAutosaveTimer() {
+		TimerTask autosave = new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					saveData();
+				}
+				catch (IOException e) {
+					System.out.println("FAILED TO AUTOSAVE!");
+					throw new Error("Failed autosave");
+				}
+			}
+		};
+		autosave_timer.scheduleAtFixedRate(autosave, AUTOSAVE_TIMER, AUTOSAVE_TIMER);
 
 	}
 
-	private static void init() {
+	public static void init() throws IOException {
 		loadData();
 		setupAutosaveTimer();
 	}
--- userdata/src/main/java/module-info.java
+++ userdata/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module mimuw.idlearn.userdata {
 	requires mimuw.idlearn.core;
+	requires gson;
 
 	exports mimuw.idlearn.userdata;
 }
