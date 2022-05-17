bb6713473cce05fb55cc4875cdfa0df5ddc0ed4e
kfernandez31 <kacper.fernandez@gmail.com>
2022-05-17 09:52:58 +0200

    Added the points button insertion and gave each scene its individual controller

---
+++ scenes/src/main/java/mimuw/idlearn/scenes/controllers/MainMenuController.java
@@ -0,0 +1,12 @@
+package mimuw.idlearn.scenes.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController extends GenericController {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
--- scenes/src/main/java/mimuw/idlearn/scenes/controllers/TaskSelectionController.java
+++ scenes/src/main/java/mimuw/idlearn/scenes/controllers/TaskSelectionController.java
@@ -2,7 +2,10 @@ package mimuw.idlearn.scenes.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import mimuw.idlearn.scenes.ResourceHandler;
 import mimuw.idlearn.userdata.DataManager;
@@ -12,28 +15,33 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class TaskSelectionSceneController extends GenericController implements Initializable {
+public class TaskSelectionController extends GenericController {
 	@FXML
 	private VBox tasksVBox;
+	@FXML
+	private BorderPane mainBorderPane;
 
 	/** Loads all the user's available tasks as clickable buttons **/
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
+		Button pointsBtn = ResourceHandler.createUserPointsButton();
+		mainBorderPane.setTop(pointsBtn);
+
 		List<String> tasks = DataManager.getUnlockedTasks();
 		double btnWidth = tasksVBox.getMaxWidth();
 
 		for (final String title : tasks) {
-			Button btn = new Button(title);
-			btn.setMaxWidth(btnWidth);
-			btn.setOnMouseClicked((event) -> {
+			Button taskBtn = new Button(title);
+			taskBtn.setMaxWidth(btnWidth);
+			taskBtn.setOnMouseClicked((event) -> {
 				System.out.println("Selected task \"" + title + "\"");
 				try {
-					enterNextScene(ResourceHandler.Task, new TaskSceneController(title));
+					enterNextScene(ResourceHandler.Task, new TaskController(title));
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			});
-			tasksVBox.getChildren().add(btn);
+			tasksVBox.getChildren().add(taskBtn);
 		}
 	}
 }
