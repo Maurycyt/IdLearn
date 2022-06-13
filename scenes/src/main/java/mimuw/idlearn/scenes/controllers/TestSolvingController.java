4eac6c30cac66d050a58c5c1f3e030b385fb23fb
kfernandez31 <kacper.fernandez@gmail.com>
2022-06-13 15:32:02 +0200

    Added text fields to the TestSolving scene, have to tweak their dimensions though

--- scenes/src/main/java/mimuw/idlearn/scenes/controllers/TestSolvingController.java
+++ scenes/src/main/java/mimuw/idlearn/scenes/controllers/TestSolvingController.java
@@ -3,7 +3,13 @@ package mimuw.idlearn.scenes.controllers;
 import javafx.beans.InvalidationListener;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import mimuw.idlearn.packages.PackageManager;
 import mimuw.idlearn.packages.ProblemPackage;
 
@@ -21,10 +27,29 @@ public class TestSolvingController extends TaskController {
         }
     }
 
+    private void submitOutput() {
+        if ((outputTextArea.getText() != null && !outputTextArea.getText().isEmpty())) {
+            System.out.println("your input: " + outputTextArea.getText());
+        } else {
+            System.out.println("You have not left a comment.");
+        }
+    }
+
+    @FXML
+    private Text inputText;
+    @FXML
+    private StackPane outputStackPane;
+    @FXML
+    private ScrollPane outputScrollPane;
+    @FXML
+    private TextArea outputTextArea;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         super.initialize(url, resourceBundle);
-        submitBtn.setOnAction(event ->  goBack(null));
+        submitBtn.setOnAction(event -> submitOutput());
         backBtn.setOnAction(event -> goBack(null));
+        outputTextArea.setFocusTraversable(false);
+        outputTextArea.maxWidthProperty().bind(outputScrollPane.widthProperty());
     }
 }
